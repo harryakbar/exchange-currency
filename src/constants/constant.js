38f6c7695c0dffdfda3ccaffb8a41b18534c6bb2
Harry Akbar Ali Munir <harryakbaram@gmail.com>
2019-05-04 23:32:59 +0700

    Add currencies list constants for wording and currency code

---
+++ src/constants/constant.js
@@ -0,0 +1,42 @@
+export const currencies = {
+  USD: {
+    currency: 'United States Dollars',
+    code: 'USD',
+  },
+  CAD: {
+    currency: 'Canadian Dollar',
+    code: 'CAD',
+  },
+  IDR: {
+    currency: 'Rupiah',
+    code: 'IDR',
+  },
+  GBP: {
+    currency: 'Pound Sterling',
+    code: 'GBP',
+  },
+  CHF: {
+    currency: 'Swiss Franc',
+    code: 'CHF',
+  },
+  SGD: {
+    currency: 'Singapore Dollar',
+    code: 'SGD',
+  },
+  INR: {
+    currency: 'Indian Rupee',
+    code: 'INR',
+  },
+  MYR: {
+    currency: 'Malaysian Ringgit',
+    code: 'MYR',
+  },
+  JPY: {
+    currency: 'Japan Yen',
+    code: 'JPY',
+  },
+  KRW: {
+    currency: 'Korean Won',
+    code: 'KRW',
+  },
+};
